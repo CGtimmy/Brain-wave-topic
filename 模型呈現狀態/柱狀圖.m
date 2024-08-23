1d258c66c764304bbaaf913d46fa62da2dec024d
CGtimmy <146343786+CGtimmy@users.noreply.github.com>
2024-08-23 14:21:47 +0800

    Create 柱狀圖.m

---
+++ 模型呈現狀態/柱狀圖.m
@@ -0,0 +1,8 @@
+% % Short baseline繪製柱狀圖
+% figure;
+% bar([beta_mean_mean_baseline_Short,alpha_mean_mean_baseline_Short]);
+% title('Short baseline 平均能量');
+% xlabel('腦波種類');
+% ylabel('平均能量');
+% set(gca, 'XTickLabel', {'beta波(12-28Hz)', 'alpha波(8-12Hz)'});
+% grid on;
